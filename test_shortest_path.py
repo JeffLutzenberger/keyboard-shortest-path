2ef9b15cb6b3bc325dd770d76e990f07bbbf5a65
Jeff Lutzenberger <jeff.lutzenberger@webfilings.com>
2013-12-15 17:17:22 -0700

    variable name fix

--- test_shortest_path.py
+++ test_shortest_path.py
@@ -1,4 +1,4 @@
-import keyboard_shortest_path as kps
+import keyboard_shortest_path as ksp
 import unittest
 
 
@@ -11,17 +11,17 @@ class test_shortest_path(unittest.TestCase):
         rows = 3
         columns = 9
 
-        commands = kps.shortest_path(rows, columns, 'jeff')
+        commands = ksp.shortest_path(rows, columns, 'jeff')
         self.assertEqual(commands, [(1, 0), (-1, 4), (0, 1), (0, 0)])
-        word = kps.commands_to_word(rows, columns, commands)
+        word = ksp.commands_to_word(rows, columns, commands)
         self.assertEqual(word, 'jeff')
 
-        commands = kps.shortest_path(rows, columns, 'zebra')
+        commands = ksp.shortest_path(rows, columns, 'zebra')
         self.assertEqual(commands,
                          [(-1, -2), (1, -3), (0, -3), (1, -2), (-1, 1)])
-        word = kps.commands_to_word(rows, columns, commands)
+        word = ksp.commands_to_word(rows, columns, commands)
         self.assertEqual(word, 'zebra')
-        #kps.print_keyboard(kps.make_keyboard(rows, columns))
+        ksp.print_keyboard(ksp.make_keyboard(rows, columns))
 
 
 if __name__ == '__main__':
